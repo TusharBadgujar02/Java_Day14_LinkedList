a63cc92f3ac3881912ee7fcf0702c899b5bdf2f5
9685959802 <tusharbadgujar0000@gmail.com>
2022-09-20 17:49:19 +0530

    Creating a Linked List

---
+++ LinkedList.java
@@ -0,0 +1,33 @@
+package bridgelabz;
+
+public class LinkedList<T> {
+    Node<T> head;
+
+    public void add(T value) {
+        Node<T> newNode = new Node<T>();
+        newNode.data = value;
+        newNode.next = null;
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> last = head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+    }
+
+    /**
+     * Method for printing the data inside the linkedlist.
+     */
+    public void show() {
+        Node<T> node = head;
+        while (node.next != null) {
+            System.out.println(node.data);
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
+}
+ 

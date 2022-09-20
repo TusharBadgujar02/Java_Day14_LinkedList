79b07acbac1c8378dab6844271cd91409aa6b550
9685959802 <tusharbadgujar0000@gmail.com>
2022-09-20 17:58:09 +0530

    Add elements at Start node

--- LinkedList.java
+++ LinkedList.java
@@ -1,33 +1,49 @@
 package bridgelabz;
 
 public class LinkedList<T> {
-    Node<T> head;
+	 Node<T> head;
+	    /**
+	     * Method for Appending the specified element to the end of this list.
+	     *
+	     * @param value
+	     */
+	    public void add(T value) {
+	        Node<T> newNode = new Node<T>();
+	        newNode.data = value;
+	        newNode.next = null;
+	        if (head == null) {
+	            head = newNode;
+	        } else {
+	            Node<T> last = head;
+	            while (last.next != null) {
+	                last = last.next;
+	            }
+	            last.next = newNode;
+	        }
+	    }
 
-    public void add(T value) {
-        Node<T> newNode = new Node<T>();
-        newNode.data = value;
-        newNode.next = null;
-        if (head == null) {
-            head = newNode;
-        } else {
-            Node<T> last = head;
-            while (last.next != null) {
-                last = last.next;
-            }
-            last.next = newNode;
-        }
-    }
+	    /**
+	     * Method for printing the data inside the linkedlist.
+	     */
+	    public void show() {
+	        Node<T> node = head;
+	        while (node.next != null) {
+	            System.out.println(node.data);
+	            node = node.next;
+	        }
+	        System.out.println(node.data);
+	    }
 
-    /**
-     * Method for printing the data inside the linkedlist.
-     */
-    public void show() {
-        Node<T> node = head;
-        while (node.next != null) {
-            System.out.println(node.data);
-            node = node.next;
-        }
-        System.out.println(node.data);
-    }
+	    /**
+	     * Method for Inserting the specific element at the start of the list.
+	     *
+	     * @param value
+	     */
+	    public void addAtStart(T value) {
+	        Node<T> newNode = new Node<>();
+	        newNode.data = value;
+	        newNode.next = head;
+	        head = newNode;
+	    }
 }
  
--- Runner.java
+++ Runner.java
@@ -5,15 +5,8 @@ public class Runner {
 	        LinkedList<Integer> numberList = new LinkedList<>();
 	        numberList.add(56);
 	        numberList.add(70);
-	        numberList.addAtIndex(1, 30);
+	        numberList.add(30);
 	        numberList.show();
-	        System.out.println("After Inserting");
-	        numberList.searchAndInsert(30,40);
-	        numberList.show();
-	        System.out.println();
-	        numberList.searchValueAndDelete(40);
-	        numberList.show();
-	        System.out.println("Size of list is : " + numberList.size());
 
 	    }
 }

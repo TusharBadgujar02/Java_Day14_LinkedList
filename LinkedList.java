782b010dd179b3c38a20c8bdfe39100a068a70fe
9685959802 <tusharbadgujar0000@gmail.com>
2022-09-20 18:10:27 +0530

    Delete the first element from linkedList

--- LinkedList.java
+++ LinkedList.java
@@ -2,6 +2,11 @@ package bridgelabz;
 
 public class LinkedList<T> {
 	 Node<T> head;
+	    /**
+	     * Method for Appending the specified element to the end of this list.
+	     *
+	     * @param value
+	     */
 	    public void add(T value) {
 	        Node<T> newNode = new Node<T>();
 	        newNode.data = value;
@@ -17,6 +22,9 @@ public class LinkedList<T> {
 	        }
 	    }
 
+	    /**
+	     * Method for printing the data inside the linkedlist.
+	     */
 	    public void show() {
 	        Node<T> node = head;
 	        while (node.next != null) {
@@ -26,11 +34,47 @@ public class LinkedList<T> {
 	        System.out.println(node.data);
 	    }
 
+	    /**
+	     * Method for Inserting the specific element at the start of the list.
+	     *
+	     * @param value
+	     */
 	    public void addAtStart(T value) {
 	        Node<T> newNode = new Node<>();
 	        newNode.data = value;
 	        newNode.next = head;
 	        head = newNode;
+	    }
+
+	    /**
+	     * Method for Inserting the data at specific index of the list.
+	     * @param index : position at which to be inserted
+	     * @param data : data to be inserted
+	     */
+	    public void addAtIndex(int index, T data) {
+	        Node<T> newNode = new Node<>();
+	        newNode.data = data;
+
+	        if (index == 0) {
+	            addAtStart(data);
+	        } else {
+	            Node<T> node = head;
+	            for (int i = 0; i < index - 1; i++) {
+	                node = node.next;
+	            }
+	            newNode.next = node.next;
+	            node.next = newNode;
+	        }
+	    }
+
+	    /**
+	     * Method for removing the first element from the list.
+	     */
+	    public void pop() {
+	        if (null != head) {
+	            Node<T> newHead = head.next;
+	            head = newHead;
+	        }
 	    }
 
 }
--- Runner.java
+++ Runner.java
@@ -3,9 +3,14 @@ package bridgelabz;
 public class Runner {
 	 public static void main(String[] args) {
 		 LinkedList<Integer> numberList = new LinkedList<>();
+		 LinkedList<Integer> numberList1 = new LinkedList<>();
 	        numberList.add(56);
 	        numberList.add(70);
-	        numberList.addAtIndex(1,30);
+	        numberList.addAtIndex(1, 30);
+	        System.out.println("Before Deleting First Element");
+	        numberList.show();
+	        System.out.println("After Deleting First Element");
+	        numberList.pop();
 	        numberList.show();
 	    }
 	}

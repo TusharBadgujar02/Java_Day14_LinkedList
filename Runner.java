33f8373f7e7047151a28fa8ec035c2f59ddf8e52
9685959802 <tusharbadgujar0000@gmail.com>
2022-09-20 18:06:29 +0530

    Insert data at specific index

--- Runner.java
+++ Runner.java
@@ -2,11 +2,10 @@ package bridgelabz;
 
 public class Runner {
 	 public static void main(String[] args) {
-	        LinkedList<Integer> numberList = new LinkedList<>();
+		 LinkedList<Integer> numberList = new LinkedList<>();
 	        numberList.add(56);
 	        numberList.add(70);
-	        numberList.add(30);
+	        numberList.addAtIndex(1,30);
 	        numberList.show();
-
 	    }
-}
+	}

df7631ca74bf6f3167e1705f56e3551b7e55caf2
9685959802 <tusharbadgujar0000@gmail.com>
2022-09-20 18:16:17 +0530

    search by value to find node

--- LinkedList.java
+++ LinkedList.java
@@ -1,96 +1,118 @@
 package bridgelabz;
 
 public class LinkedList<T> {
-	 Node<T> head;
+	Node<T> head;
 
-	    /**
-	     * Method for Appending the specified element to the end of this list.
-	     *
-	     * @param value
-	     */
-	    public void add(T value) {
-	        Node<T> newNode = new Node<T>();
-	        newNode.data = value;
-	        newNode.next = null;
-	        if (head == null) {
-	            head = newNode;
-	        } else {
-	            Node<T> last = head;
-	            while (last.next != null) {
-	                last = last.next;
-	            }
-	            last.next = newNode;
-	        }
-	    }
+    /**
+     * Method for Appending the specified element to the end of this list.
+     *
+     * @param value
+     */
+    public void add(T value) {
+        Node<T> newNode = new Node<T>();
+        newNode.data = value;
+        newNode.next = null;
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> last = head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+    }
 
-	    /**
-	     * Method for printing the data inside the linkedlist.
-	     */
-	    public void show() {
-	        Node<T> node = head;
-	        while (node.next != null) {
-	            System.out.println(node.data);
-	            node = node.next;
-	        }
-	        System.out.println(node.data);
-	    }
+    /**
+     * Method for printing the data inside the linkedlist.
+     */
+    public void show() {
+        Node<T> node = head;
+        while (node.next != null) {
+            System.out.println(node.data);
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
 
-	    /**
-	     * Method for Inserting the specific element at the start of the list.
-	     *
-	     * @param value
-	     */
-	    public void addAtStart(T value) {
-	        Node<T> newNode = new Node<>();
-	        newNode.data = value;
-	        newNode.next = head;
-	        head = newNode;
-	    }
+    /**
+     * Method for Inserting the specific element at the start of the list.
+     *
+     * @param value
+     */
+    public void addAtStart(T value) {
+        Node<T> newNode = new Node<>();
+        newNode.data = value;
+        newNode.next = head;
+        head = newNode;
+    }
 
-	    /**
-	     * Method for Inserting the data at specific index of the list.
-	     *
-	     * @param index : position at which to be inserted
-	     * @param data  : data to be inserted
-	     */
-	    public void addAtIndex(int index, T data) {
-	        Node<T> newNode = new Node<>();
-	        newNode.data = data;
+    /**
+     * Method for Inserting the data at specific index of the list.
+     *
+     * @param index : position at which to be inserted
+     * @param data  : data to be inserted
+     */
+    public void addAtIndex(int index, T data) {
+        Node<T> newNode = new Node<>();
+        newNode.data = data;
 
-	        if (index == 0) {
-	            addAtStart(data);
-	        } else {
-	            Node<T> node = head;
-	            for (int i = 0; i < index - 1; i++) {
-	                node = node.next;
-	            }
-	            newNode.next = node.next;
-	            node.next = newNode;
-	        }
-	    }
+        if (index == 0) {
+            addAtStart(data);
+        } else {
+            Node<T> node = head;
+            for (int i = 0; i < index - 1; i++) {
+                node = node.next;
+            }
+            newNode.next = node.next;
+            node.next = newNode;
+        }
+    }
 
-	    /**
-	     * Method for removing the first element from the list.
-	     */
-	    public void pop() {
-	        if (null != head) {
-	            Node<T> newHead = head.next;
-	            head = newHead;
-	        }
-	    }
+    /**
+     * Method for removing the first element from the list.
+     */
+    public void pop() {
+        if (null != head) {
+            Node<T> newHead = head.next;
+            head = newHead;
+        }
+    }
 
-	    /**
-	     * Method for removing the last element from the list.
-	     */
-	    public void popLast() {
-	        Node<T> tail = head;
-	        Node<T> prevNode = head;
-	        while (tail.next != null) {
-	            prevNode = tail;
-	            tail = tail.next;
-	        }
-	        prevNode.next = null;
-	    }
+    /**
+     * Method for removing the last element from the list.
+     */
+    public void popLast() {
+        Node<T> tail = head;
+        Node<T> prevNode = head;
+        while (tail.next != null) {
+            prevNode = tail;
+            tail = tail.next;
+        }
+        prevNode.next = null;
+    }
 
+    /**
+     * Method for searching the node by value.
+     * @param value : Value to be searched
+     * @return
+     */
+    public int searchByValue(T value) {
+        Node<T> currNode = head;
+        int index = 0;
+        if (null != currNode) {
+            while ((null != currNode.next) || (null != currNode.data)) {
+                if (currNode.data == value) {
+                    break;
+                }
+                currNode = currNode.next;
+                if (null == currNode) {
+                    return -1;
+                }
+                index++;
+            }
+        }
+        return index;
+    }
 }
  
--- Runner.java
+++ Runner.java
@@ -3,14 +3,11 @@ package bridgelabz;
 public class Runner {
 	 public static void main(String[] args) {
 		 LinkedList<Integer> numberList = new LinkedList<>();
-		 LinkedList<Integer> numberList1 = new LinkedList<>();
 	        numberList.add(56);
 	        numberList.add(70);
 	        numberList.addAtIndex(1, 30);
-	        System.out.println("Before Deleting First Element");
 	        numberList.show();
-	        System.out.println("After Deleting First Element");
-	        numberList.pop();
-	        numberList.show();
+	        String searchresult = (numberList.searchByValue(30) == -1) ? "value not found in the list" : "value found at index " + numberList.searchByValue(30);
+	        System.out.println(searchresult);
 	    }
 	}
